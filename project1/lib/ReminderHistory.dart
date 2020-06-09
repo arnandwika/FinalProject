2deba80c1382d7fadb32060ec63898f47e2923e8
nadiacarrisa <stephanie.nadia@ti.ukdw.ac.id>
2020-06-09 21:52:04 +0700

    History Reminder UI

--- project1/lib/ReminderHistory.dart
+++ project1/lib/ReminderHistory.dart
@@ -30,61 +30,96 @@ class ReminderHistory extends StatelessWidget{
       OpenDb();
     return Scaffold(
       appBar: AppBar(
+        title: Text("Detail Riwayat Acara"),
         backgroundColor: blue,
       ),
-      body: Container(
+      body: Padding(
+        padding: EdgeInsets.all(8),
         child: Column(
           children: <Widget>[
-            Text(
-              judul,
-              style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 30
-              ),
-            ),
-            Text(
-              tanggal,
-              style: TextStyle(
-                fontSize: 14,
-              ),
-            ),
-            SingleChildScrollView(
-              child: Container(
-                height: 200,
-                child: Text(
-                   isi
-                ),
-              ),
-            ),
-            Container(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Expanded(
-                    child: RaisedButton(
-                      onPressed: () async => {
-                        await hapusData(id),
-                        await hapusFirestore(id),
-                        Navigator.pop(context),
-                        Navigator.pop(context),
-                        await OpenDb(),
-                        Navigator.pushNamed(context, '/history'),
-
-                      },
-                      color: blue,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: new BorderRadius.circular(18.0),
-                      ),
-                      child: Text(
-                        "Hapus",
-                        style: TextStyle(
-                            fontSize: 20
-                        ),),
+            Row(
+              children: <Widget>[
+                SizedBox(
+                  width: 150,
+                  child: Text(
+                    "Nama Acara",
+                    style: TextStyle(
+                      fontSize: 20,
                     ),
                   ),
-                ],
+                ),
+                Text(
+                  " : "+judul,
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            Row(
+              children: <Widget>[
+                SizedBox(
+                  width: 150,
+                  child: Text(
+                    "Tanggal & Jam ",
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+                Text(
+                  " : "+tanggal,
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            Row(
+              children: <Widget>[
+                SizedBox(
+                  width: 150,
+                  height: 150,
+                  child: Text(
+                    "Deskripsi ",
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 150,
+                  child: Text(
+                    " : "+isi,
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                )
+              ],
+            ),
+            SizedBox(
+              width: 200,
+              height: 50,
+              child: RaisedButton(
+                color: Colors.red,
+                child: Text("Hapus", style: TextStyle(color: white, fontSize: 20),),
+                onPressed: () async => {
+                  await hapusData(id),
+                  await hapusFirestore(id),
+                  Navigator.pop(context),
+                  Navigator.pop(context),
+                  await OpenDb(),
+                  Navigator.pushNamed(context, '/history'),
+                },
               ),
-            )
+            ),
           ],
         ),
       ),
