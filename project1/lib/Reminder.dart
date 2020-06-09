bf591c584dde5c9face52b4cfdb51fbaa88e723b
nadiacarrisa <stephanie.nadia@ti.ukdw.ac.id>
2020-06-09 23:29:55 +0700

    Date Format

--- project1/lib/Reminder.dart
+++ project1/lib/Reminder.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:date_format/date_format.dart';
 import 'package:flutter/material.dart';
 
 import 'Edit.dart';
@@ -28,80 +29,111 @@ class Reminder extends StatelessWidget{
       OpenDb();
     return Scaffold(
       appBar: AppBar(
-        title: Text("Detail Acara"),
+        title: Text("Detail "),
         backgroundColor: blue,
       ),
       body: Padding(
         padding: EdgeInsets.all(8),
-        child: Column(
+        child: ListView(
           children: <Widget>[
-            Row(
-              children: <Widget>[
-                SizedBox(
-                  width: 150,
-                  child: Text(
-                    "Nama Acara",
-                    style: TextStyle(
-                      fontSize: 20,
-                    ),
-                  ),
-                ),
-                Text(
-                  " : "+judul,
-                  style: TextStyle(
-                    fontSize: 20,
-                  ),
-                ),
-              ],
+            Text(
+                "Detail Acara: ",
+                style: TextStyle(fontSize: 23,fontWeight: FontWeight.bold)
             ),
             SizedBox(
-              height: 8,
+              height: 15,
             ),
-            Row(
+            Column(
               children: <Widget>[
-                SizedBox(
-                  width: 150,
-                  child: Text(
-                    "Tanggal & Jam ",
-                    style: TextStyle(
-                      fontSize: 20,
+                Row(
+                  children: <Widget>[
+                    SizedBox(
+                      width: 150,
+                      child: Text(
+                        "Nama Acara",
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-                Text(
-                  " : "+tanggal,
-                  style: TextStyle(
-                    fontSize: 20,
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 8,
-            ),
-            Row(
-              children: <Widget>[
-                SizedBox(
-                  width: 150,
-                  height: 150,
-                  child: Text(
-                    "Deskripsi ",
-                    style: TextStyle(
-                      fontSize: 20,
+                    Text(
+                      " : "+judul,
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
                     ),
-                  ),
+                  ],
                 ),
                 SizedBox(
-                  height: 150,
-                  child: Text(
-                    " : "+isi,
-                    style: TextStyle(
-                      fontSize: 20,
+                  height: 8,
+                ),
+                Row(
+                  children: <Widget>[
+                    SizedBox(
+                      width: 150,
+                      child: Text(
+                        "Tanggal Acara ",
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
                     ),
-                  ),
-                )
-              ],
-            ),
+                    Text(
+                      " : "+formatDate(DateTime.parse(tanggal),[d," ",MM," ",yyyy]).toString(),
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 8,
+                ),
+                Row(
+                  children: <Widget>[
+                    SizedBox(
+                      width: 150,
+                      child: Text(
+                        "Jam Acara ",
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      " : "+formatDate(DateTime.parse(tanggal),[HH,":",nn]).toString(),
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 8,
+                ),
+                Row(
+                  children: <Widget>[
+                    SizedBox(
+                      width: 150,
+                      height: 150,
+                      child: Text(
+                        "Deskripsi ",
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 150,
+                      child: Text(
+                        " : "+isi,
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
 //            RaisedButton(
 //                child: Text("Foto Foto"),
 //                onPressed: () async{
@@ -111,38 +143,40 @@ class Reminder extends StatelessWidget{
 //                  ));
 //                }
 //            ),
-            SizedBox(
-              width: 200,
-              height: 50,
-              child: RaisedButton(
-                color: blue,
-                child: Text("Edit", style: TextStyle(color: white, fontSize: 20),),
-                onPressed: () => {
-                  Navigator.push(context, MaterialPageRoute(
-                    builder: (context) =>Edit(id,judul, tanggal, isi),
-                  )),
-                },
-              ),
-            ),
-            SizedBox(
-              height: 8,
-            ),
-            SizedBox(
-              width: 200,
-              height: 50,
-              child: RaisedButton(
-                color: Colors.red,
-                child: Text("Hapus", style: TextStyle(color: white, fontSize: 20),),
-                onPressed: () async => {
-                  await hapusData(id),
-                  await hapusFirestore(id),
-                  Navigator.pop(context),
-                  Navigator.pop(context),
-                  await OpenDb(),
-                  Navigator.pushNamed(context, '/home'),
-                },
-              ),
-            ),
+                SizedBox(
+                  width: 200,
+                  height: 50,
+                  child: RaisedButton(
+                    color: blue,
+                    child: Text("Edit", style: TextStyle(color: white, fontSize: 20),),
+                    onPressed: () => {
+                      Navigator.push(context, MaterialPageRoute(
+                        builder: (context) =>Edit(id,judul, tanggal, isi),
+                      )),
+                    },
+                  ),
+                ),
+                SizedBox(
+                  height: 8,
+                ),
+                SizedBox(
+                  width: 200,
+                  height: 50,
+                  child: RaisedButton(
+                    color: Colors.red,
+                    child: Text("Hapus", style: TextStyle(color: white, fontSize: 20),),
+                    onPressed: () async => {
+                      await hapusData(id),
+                      await hapusFirestore(id),
+                      Navigator.pop(context),
+                      Navigator.pop(context),
+                      await OpenDb(),
+                      Navigator.pushNamed(context, '/home'),
+                    },
+                  ),
+                ),
+              ],
+            )
           ],
         ),
       ),
